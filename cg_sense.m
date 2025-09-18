d1f8a220fd9ea66d3219a9358bcc5423e4836073
Rex Fung <rexfung@umich.edu>
2025-09-18 17:40:33 -0400

    2025/09/18 save

--- cg_sense.m
+++ cg_sense.m
@@ -11,7 +11,7 @@ Nx = 120; Ny = 120; Nz = 40; Ncoils = 32; Nframes = 20;
 %% Recon with CG-SENSE
 tic;
 img = zeros(Nx,Ny,Nz,Nframes);
-parfor frame = 1:Nframes
+for frame = 1:Nframes
     fprintf('Reconstructing frame %d\n', round(frame));
     data = squeeze(kdata.ksp_zf(:,:,:,:,frame));
     img(:,:,:,frame) = bart('pics -l1 -r0.001', data, smaps);
